a24ae52130af5e4ac25ca630d2ccd8b4401096a6
sujauddin <sujauddinkazi282538@gmail.com>
2023-02-04 19:10:17 +0530

    First commit

---
+++ src/Components/Index.jsx
@@ -0,0 +1,10 @@
+import React  from "react";
+
+export const Index =() => {
+    return (
+        <div>
+            <h1>This is my first component</h1>
+            <img src="https://mobirise.com/engine/data/upload/2016/03/mobirise-main-theme.png" alt="" />
+        </div>
+    )
+}
